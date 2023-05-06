af952b59a486cce104e58574d22475f73f3659ca
sergey s <som-ana@yandex.ru>
2023-05-06 20:08:23 +0300

    initial commit

---
+++ src/main/java/ru/netology/Radio/Control/RadioChannels.java
@@ -0,0 +1,39 @@
+package ru.netology.Radio.Control;
+
+public class RadioChannels {
+    private int currentRadioStationNumber; //текущий номер радиостанции
+
+    public int getCurrentRadioStationNumber() { //отдать текущий номер радиостанции
+        return currentRadioStationNumber;
+    }
+
+    public void setCurrentRadioStationNumber(int newCurrentRadioStationNumber) { //установить новый текущий номер радиостанции
+        if (newCurrentRadioStationNumber < 0) {
+            newCurrentRadioStationNumber = 9;
+        }
+        if (newCurrentRadioStationNumber > 9) {
+            newCurrentRadioStationNumber = 0;
+        }
+        currentRadioStationNumber = newCurrentRadioStationNumber;
+    }
+
+    public void next1ChannelNumber() {
+        int next = getCurrentRadioStationNumber() + 1;
+        setCurrentRadioStationNumber(next);
+    }
+
+    public void prev1ChannelNumber() {
+        int prev = getCurrentRadioStationNumber() - 1;
+        setCurrentRadioStationNumber(prev);
+    }
+
+    public void setTheRadioStationNumber(int newCurrentRadioStationNumber) {//установить номер станции в диапазоне от 0 до 9
+        if (newCurrentRadioStationNumber < 0) {
+            return;
+        }
+        if (newCurrentRadioStationNumber > 9) {
+            return;
+        }
+        currentRadioStationNumber = newCurrentRadioStationNumber;
+    }
+}
