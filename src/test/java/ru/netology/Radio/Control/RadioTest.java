0cf6e4245c22ea0588d9d69055d61cd7a83abc32
sarian-s <127656330+sarian-s@users.noreply.github.com>
2023-05-10 00:57:50 +0300

    Create RadioTest.java

---
+++ src/test/java/ru/netology/Radio/Control/RadioTest.java
@@ -0,0 +1,151 @@
+package ru.netology.Radio.Control;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class RadioTest {
+    @Test
+    public void channelNumber() {               //установить текущий номер радиостанции
+        Radio channel = new Radio();        //новый объект Radio
+        channel.setCurrentStationNumber(5);//допустимые номера радиостанций от 0 до 9
+
+        int expected = 5;//ожидаемый номер радиостанции
+        int actual = channel.getCurrentStationNumber();//фактический номер радиостанции
+
+        Assertions.assertEquals(expected, actual);//сравнение ОР и ФР
+    }
+    @Test
+    public void channelNumberLowerBound() {               //установить текущий номер радиостанции нижняя граница
+        Radio channel = new Radio();              //новый объект Radio
+        channel.setTheStationNumber(0);  //допустимые номера радиостанций от 0 до 9
+
+        int expected = 0;                       //ожидаемый номер радиостанции
+        int actual = channel.getCurrentStationNumber();//фактический номер радиостанции
+
+        Assertions.assertEquals(expected, actual);//сравнение ОР и ФР
+    }
+    @Test
+    public void channelNumberUpperBound() {               //установить текущий номер радиостанции верхняя граница
+        Radio channel = new Radio();              //новый объект Radio
+        channel.setTheStationNumber(9);  //допустимые номера радиостанций от 0 до 9
+
+        int expected = 9;                       //ожидаемый номер радиостанции
+        int actual = channel.getCurrentStationNumber();//фактический номер радиостанции
+
+        Assertions.assertEquals(expected, actual);//сравнение ОР и ФР
+    }
+
+    @Test
+    public void shouldNotSetStationNumberAboveMax() {//установить текущий номер радиостанции выше максимального
+        Radio channel = new Radio();
+        channel.setTheStationNumber(10);         //недопустимые номера: кроме от 0 до 9
+
+        int expected = 0;
+        int actual = channel.getCurrentStationNumber();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldNotSetStationNumberAboveMin() {//установить текущий номер радиостанции ниже минимального
+        Radio channel = new Radio();
+        channel.setTheStationNumber(-1);         //недопустимые номера: кроме от 0 до 9
+
+        int expected = 0;
+        int actual = channel.getCurrentStationNumber();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void nextChannel() {                      //установить текущий номер на следующий номер
+        Radio channel = new Radio();
+        channel.setCurrentStationNumber(6);
+        channel.next1ChannelNumber();
+
+        int expected = 7;
+        int actual = channel.getCurrentStationNumber();
+        Assertions.assertEquals(expected, actual);
+    }
+    @Test
+    public void nextChannelCarousel() {                      //установить текущий номер на следующий номер по карусели
+        Radio channel = new Radio();
+        channel.setCurrentStationNumber(9);              //установить текущий номер 9
+        channel.next1ChannelNumber();
+
+        int expected = 0;
+        int actual = channel.getCurrentStationNumber();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void prevChannel() {                      //установить текущий номер на предыдущий номер
+        Radio channel = new Radio();
+        channel.setCurrentStationNumber(7);
+        channel.prev1ChannelNumber();
+
+        int expected = 6;
+        int actual = channel.getCurrentStationNumber();
+        Assertions.assertEquals(6, channel.getCurrentStationNumber());
+    }
+    @Test
+    public void prevChannelCarousel() {                      //установить текущий номер на предыдущий номер по карусели
+        Radio channel = new Radio();
+        channel.setCurrentStationNumber(0);      //установить текущий номер 0
+        channel.prev1ChannelNumber();
+
+        Assertions.assertEquals(9, channel.getCurrentStationNumber());
+    }
+    @Test
+    public void setTheVolume() {                        //установить громкость
+        Radio volume = new Radio();   //новый объект уровень громкости звука
+        volume.setCurrentVolume(101);
+
+        int expected = 0;
+        int actual = volume.getCurrentVolume();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void increaseTheVolume() {
+        Radio volume = new Radio();
+        volume.setCurrentVolume(99);                   //текущая громкость звука (допустимые значения от 0 до 100)
+        volume.increaseVolume();                        //громкость звука увеличить на 1
+
+        int expected = 100;
+        int actual = volume.getCurrentVolume();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void increaseTheVolumeMax() {
+        Radio volume = new Radio ();
+        volume.setCurrentVolume(100);                   //текущая громкость звука (допустимые значения от 0 до 100)
+        volume.increaseVolume();                        //громкость звука увеличить на 1
+
+        int expected = 100;
+        int actual = volume.getCurrentVolume();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void lowerTheVolume() {
+        Radio  volume = new Radio ();
+        volume.setCurrentVolume(1);                     //текущая громкость звука (допустимые значения от 0 до 100)
+        volume.lowerVolume();                           //громкость звука уменьшить на 1
+
+        int expected = 0;
+        int actual = volume.getCurrentVolume();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void lowerTheVolumeMin() {
+        Radio  volume = new Radio ();
+        volume.setCurrentVolume(-1);                     //текущая громкость звука (допустимые значения от 0 до 100)
+        volume.lowerVolume();                           //громкость звука уменьшить на 1
+
+        int expected = 0;
+        int actual = volume.getCurrentVolume();
+        Assertions.assertEquals(expected, actual);
+    }
+
+}
